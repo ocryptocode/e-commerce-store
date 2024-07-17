f1e684f5d15925723850f47395fd8fb0390e9d28
Oussama Rafouk <156216265+ocryptocode@users.noreply.github.com>
2024-07-17 18:42:23 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import ProductList from './components/ProductList';
+import ProductDetail from './components/ProductDetail';
+import Cart from './components/Cart';
+import Checkout from './components/Checkout';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-x          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <header className="App-header">
+                    <h1>E-Commerce Store</h1>
+                </header>
+                <main>
+                    <Switch>
+                        <Route exact path="/" component={ProductList} />
+                        <Route path="/product/:id" component={ProductDetail} />
+                        <Route path="/cart" component={Cart} />
+                        <Route path="/checkout" component={Checkout} />
+                    </Switch>
+                </main>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
+
